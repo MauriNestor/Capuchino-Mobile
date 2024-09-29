11ca3dd99ca47664bd990032417ae161f38fa62d
MauriNestor <mauric845apaza@gmail.com>
2024-09-28 23:45:00 -0400

    add: modelos para el parseo de datos

--- app/src/main/java/scesi/capuchino/ui/models/Schedule.kt
+++ app/src/main/java/scesi/capuchino/ui/models/Schedule.kt
@@ -1,4 +1,28 @@
 package scesi.capuchino.ui.models
 
-class Schedule {
-}
+data class Schedule(
+    val day: String,
+    val start: String,
+    val end: String,
+    val duration: Int,
+    val room: String,
+    val teacher: String,
+    val isClass: Boolean
+)
+
+data class Group(
+    val code: String,
+    val teacher: String,
+    val schedule: List<Schedule>
+)
+
+data class Subject(
+    val code: Int,
+    val name: String,
+    val groups: List<Group>
+)
+
+data class Course(
+    val code: String,
+    val subjects: List<Subject>
+)
